e87131d3bb97e011c0ae2514c50539e557ba1262
Sandeep-Sanju <sandeeptharalla7@gmail.com>
2025-08-31 23:51:15 +0530

    Authentication Routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ import mongoose from "mongoose";
 import{ connectToSocket} from "./controllers/socketManager.js";
 
 import cors from "cors";
+import userRoutes from "./routers/users.routes.js"
 
 const app = express();
 const server = createServer(app);
@@ -18,12 +19,15 @@ app.use(cors());
 app.use(express.json({ limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb",extended: true}));
 
+app.use("/api/v1/users", userRoutes);
+
+
 app.get("/home",(req,res) => {
     return res.json({"hello":"world"})
 });
 
 const start = async () => {
-   const connectionDb = await mongoose.connect("your_mongodb_link")
+   const connectionDb = await mongoose.connect("your_mongo_db_URL")
    console.log(`Mongo Connected DB Host: ${connectionDb.connection.host}`)
         server.listen(app.get("port"), () =>{
         console.log("listening to the port 8000")
