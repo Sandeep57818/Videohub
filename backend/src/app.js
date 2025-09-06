2143697c2c8b9d1401f683f2d612c9bc20178feb
Sandeep-Sanju <sandeeptharalla7@gmail.com>
2025-09-07 01:20:35 +0530

    removed the unnecessary part

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,36 +1,9 @@
-import express from "express";
-import {createServer} from "node:http";
+import React from 'react'
 
-
-import {Server} from "socket.io";
-
-import mongoose from "mongoose";
-import{ connectToSocket} from "./controllers/socketManager.js";
-
-import cors from "cors";
-import userRoutes from "./routers/users.routes.js"
-
-const app = express();
-const server = createServer(app);
-const io = connectToSocket(server);
-
-app.set("port",(process.env.PORT || 8000))
-app.use(cors());
-app.use(express.json({ limit: "40kb"}));
-app.use(express.urlencoded({limit: "40kb",extended: true}));
-
-app.use("/api/v1/users", userRoutes);
-
-
-app.get("/home",(req,res) => {
-    return res.json({"hello":"world"})
-});
-
-const start = async () => {
-   const connectionDb = await mongoose.connect("mongodb+srv://Video_hub:mongodb.123@videohub.fl2ztwk.mongodb.net/")
-   console.log(`Mongo Connected DB Host: ${connectionDb.connection.host}`)
-        server.listen(app.get("port"), () =>{
-        console.log("listening to the port 8000")
-    });
+export default function app() {
+  return (
+    <div>
+      
+    </div>
+  )
 }
-start();
